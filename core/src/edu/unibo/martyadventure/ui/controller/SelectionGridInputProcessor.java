9c3cfab8c185cc95750c18252b218f3dd7d2e469
damiano101 <damiano.morandi@studio.unibo.it>
2021-05-04 14:51:37 +0200

    feature: add invert option to grid input processor axes

--- core/src/edu/unibo/martyadventure/ui/controller/SelectionGridInputProcessor.java
+++ core/src/edu/unibo/martyadventure/ui/controller/SelectionGridInputProcessor.java
@@ -11,6 +11,9 @@ import com.badlogic.gdx.Input;
  */
 public class SelectionGridInputProcessor extends SelectionInputProcessor<Pair<Integer, Integer>> {
 
+    private final int xAxisMultiplier;
+    private final int yAxisMultiplier;
+    
     @Override
     protected boolean isWithinBounds(Pair<Integer, Integer> candidate, Pair<Integer, Integer> bounds) {
         final int x = candidate.getValue0();
@@ -23,19 +26,22 @@ public class SelectionGridInputProcessor extends SelectionInputProcessor<Pair<In
     protected Optional<Pair<Integer, Integer>> updateSelection(int keycode, Pair<Integer, Integer> current) {
         switch (keycode) {
         case Input.Keys.UP:
-            return Optional.of(current.setAt0(current.getValue0() + 1));
+            return Optional.of(current.setAt0(current.getValue0() + 1 * this.xAxisMultiplier));
         case Input.Keys.DOWN:
-            return Optional.of(current.setAt0(current.getValue0() - 1));
+            return Optional.of(current.setAt0(current.getValue0() - 1 * this.xAxisMultiplier));
         case Input.Keys.LEFT:
-            return Optional.of(current.setAt1(current.getValue1() - 1));
+            return Optional.of(current.setAt1(current.getValue1() - 1 * this.yAxisMultiplier));
         case Input.Keys.RIGHT:
-            return Optional.of(current.setAt1(current.getValue1() + 1));
+            return Optional.of(current.setAt1(current.getValue1() + 1 * this.yAxisMultiplier));
         default:
             return Optional.empty();
         }
     }
 
-    public SelectionGridInputProcessor(ControllableSelection<Pair<Integer, Integer>> selectionGrid) {
+    public SelectionGridInputProcessor(ControllableSelection<Pair<Integer, Integer>> selectionGrid,
+            final boolean invertXAxis, final boolean invertYAxis) {
         super(selectionGrid, new Pair<Integer, Integer>(0, 0), Input.Keys.INSERT);
+        this.xAxisMultiplier = invertXAxis ? -1 : 1;
+        this.yAxisMultiplier = invertYAxis ? -1 : 1;
     }
 }
