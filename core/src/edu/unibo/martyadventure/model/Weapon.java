e8ca1f14411258d705df2365279fa7a6455cc0e5
Lorenzo Bombardini <lorenzo.bombardini@studio.unibo.it>
2021-05-06 16:24:52 +0200

    feature: create Weapon class

---
+++ core/src/edu/unibo/martyadventure/model/Weapon.java
@@ -0,0 +1,30 @@
+package edu.unibo.martyadventure.model;
+
+
+public class Weapon implements WeaponFactory {
+    
+    String name;
+    String type;
+    double damageMultiplier;
+    Move moveSet;
+    
+    public Weapon(String name, String type, double damageMultiplier, Move moveSet) {
+        this.name = name;
+        this.type = type;
+        this.damageMultiplier = damageMultiplier;
+        this.moveSet = moveSet;
+    }
+
+    @Override
+    public Weapon createWeapon(Weapon weapon, Move m1, Move m2, Move m3, Move m4) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Weapon createRandomWeapon(Weapon weapon) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
