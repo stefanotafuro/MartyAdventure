552bed2fbfca4392395236ef8bde21bc33943273
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-07 17:33:57 +0200

    started TestMapManager and Map

---
+++ core/src/edu/unibo/martyadventure/view/MapManager.java
@@ -0,0 +1,12 @@
+package edu.unibo.martyadventure.view;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Vector2;
+
+public class MapManager {
+
+    private Vector2 playerStartPosition;
+    
+    private TiledMap currentMap;
+    
+}
---
+++ core/src/test/edu/unibo/martyadventure/view/TestMapManager.java
@@ -0,0 +1,13 @@
+package test.edu.unibo.martyadventure.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMapManager {
+
+    string mapName1
+    
+    @Test
+    void TestLoadMap() {
+        
+    }
+}
