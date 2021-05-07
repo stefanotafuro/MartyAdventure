342a429b70c799f07933bddb2b0a508f72f65300
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-07 18:08:04 +0200

    feature create TestMapManager

--- core/src/edu/unibo/martyadventure/view/MapManager.java
+++ core/src/edu/unibo/martyadventure/view/MapManager.java
@@ -1,5 +1,6 @@
 package edu.unibo.martyadventure.view;
 
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Vector2;
 
@@ -8,5 +9,29 @@ public class MapManager {
     private Vector2 playerStartPosition;
     
     private TiledMap currentMap;
+    private String currentMapName;
     
+    
+    public MapManager() {
+        
+    }
+
+    public TiledMap getCurrentMap() {
+        // TODO Auto-generated method stub
+        return new TiledMap();
+    }
+
+    public String getCurrentMapName() {
+        return currentMapName;
+    }
+
+    public void loadMap(String mapName) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public MapLayer getCollisionLayer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
--- core/src/test/edu/unibo/martyadventure/view/TestMapManager.java
+++ core/src/test/edu/unibo/martyadventure/view/TestMapManager.java
@@ -1,13 +1,45 @@
 package test.edu.unibo.martyadventure.view;
 
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+
+import edu.unibo.martyadventure.view.MapManager;
 
 public class TestMapManager {
 
-    string mapName1
+    String map1 = "map1";
+    String map2 = "map1";
+    String map3 = "map1";
+    
+    MapManager manager = new MapManager();
+    
+    void TestLoadingMap(String MapName) {
+        manager.loadMap(map1);
+        TiledMap map = manager.getCurrentMap();
+        assertNotNull(map);
+        assertEquals(map1, manager.getCurrentMapName());
+    }
+    
+    void TestLoadingLayers(String MapName) {
+        manager.loadMap(map1);
+        assertNotNull(manager.getCollisionLayer());
+    }
     
     @Test
-    void TestLoadMap() {
-        
+    void TestAllMaps() {
+        TestLoadingMap(map1);
+        TestLoadingMap(map2);
+        TestLoadingMap(map3);
+    }
+    
+    @Test
+    void TestAllLayers() {
+        TestLoadingLayers(map1);
+        TestLoadingLayers(map2);
+        TestLoadingLayers(map3);
     }
 }
