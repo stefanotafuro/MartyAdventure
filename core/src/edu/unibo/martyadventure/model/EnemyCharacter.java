b9db87d81ee6098cdae300f83a15cd58bf04b0fa
stefanotafuro <stefano.tafuro@studio.unibo.it>
2021-05-07 15:11:41 +0200

    Feature: update EnemyCharacter(add getter and setter)

--- core/src/edu/unibo/martyadventure/model/EnemyCharacter.java
+++ core/src/edu/unibo/martyadventure/model/EnemyCharacter.java
@@ -1,5 +1,14 @@
 package edu.unibo.martyadventure.model;
 
 public class EnemyCharacter extends Character{
-	private Weapon drpoitem;
+	
+	private Weapon dropitem;
+
+	public Weapon getDropitem() {
+		return dropitem;
+	}
+
+	public void setDropitem(Weapon dropitem) {
+		this.dropitem = dropitem;
+	}
 }
