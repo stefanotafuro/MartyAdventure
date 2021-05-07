2be310c12ecf26eb1a2d0416659a00e71639005a
Lorenzo Bombardini <lorenzo.bombardini@studio.unibo.it>
2021-05-07 15:01:51 +0200

    feature: modify createRandomWeapon

--- core/src/edu/unibo/martyadventure/model/WeaponFactory.java
+++ core/src/edu/unibo/martyadventure/model/WeaponFactory.java
@@ -7,24 +7,19 @@ public class WeaponFactory {
     
     
     Weapon createWeapon(Weapon weapon, Move move1, Move move2, Move move3, Move move4) {
-        List<Move> moveList = new ArrayList<>();
-        moveList.add(move1);
-        moveList.add(move2);
-        moveList.add(move3);
-        moveList.add(move4);
+        List<Move> moveList = new ArrayList<>(List.of(move1, move2, move3, move4));
         weapon.setMoveList(moveList);
         return weapon;        
     }
     
     Weapon createRandomWeapon(Weapon weapon) {
-        List<Move> moveList = new ArrayList<>();
-        moveList.add(Move.getRandomMove());
-        moveList.add(Move.getRandomMove());
-        moveList.add(Move.getRandomMove());
-        moveList.add(Move.getRandomMove());
+        List<Move> moveList = new ArrayList<>(List.of(Move.getRandomMove(), Move.getRandomMove(),
+                                                    Move.getRandomMove(), Move.getRandomMove()));
         weapon.setMoveList(moveList);
         return weapon;        
     }
+    
+    
 
     /*Weapon createMeleeWeapon(Weapon weapon) {
         
