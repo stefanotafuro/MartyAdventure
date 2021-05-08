ad1796431d07ae522873162eb455474b91582720
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-08 13:03:07 +0200

    fix added Gdx.app global istance

--- desktop/src/edu/unibo/martyadventure/desktop/DesktopLauncher.java
+++ desktop/src/edu/unibo/martyadventure/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package edu.unibo.martyadventure.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import edu.unibo.martyadventure.MartyAdventureGame;
@@ -7,6 +8,8 @@ import edu.unibo.martyadventure.MartyAdventureGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        new Lwjgl3Application(new MartyAdventureGame(), config);
+        
+        Lwjgl3Application app = new Lwjgl3Application(new MartyAdventureGame(), config);
+        Gdx.app = app;
     }
 }
