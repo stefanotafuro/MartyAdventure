05bcebdabdfb42e1aa1c8eb1fbe2606057e2409c
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-12 12:16:38 +0200

    fix static methods and loadMap

--- core/src/edu/unibo/martyadventure/view/Toolbox.java
+++ core/src/edu/unibo/martyadventure/view/Toolbox.java
@@ -3,6 +3,7 @@ package edu.unibo.martyadventure.view;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
 public class Toolbox {
 
@@ -16,39 +17,50 @@ public class Toolbox {
     }
 
 
-    public void unloadAsset(String filePath) {
+    public static void unloadAsset(String filePath) {
         Toolbox.assetManager.unload(filePath);
     }
 
-    public float loadCompletion() {
+    public static float loadCompletion() {
         return Toolbox.assetManager.getProgress();
     }
 
-    public int queuedAssetCount() {
+    public static int queuedAssetCount() {
         return Toolbox.assetManager.getQueuedAssets();
     }
 
-    public boolean updateAssetLoading() {
+    public static boolean updateAssetLoading() {
         return Toolbox.assetManager.update();
     }
 
-    public boolean isAssetLoaded(String filePath) {
+    public static boolean isAssetLoaded(String filePath) {
         return Toolbox.assetManager.isLoaded(filePath);
     }
 
-    public void loadMap(String mapPath) {
+    public static void loadMap(String mapPath) {
+        
+        if (mapPath == null || mapPath.isEmpty()) {
+            return;
+        }
+        
+        if( Toolbox.assetManager.isLoaded(mapPath) ){
+            return;
+        }
+        
+        Toolbox.assetManager.setLoader(TiledMap.class, new TmxMapLoader());
         Toolbox.assetManager.load(mapPath, TiledMap.class);
+        Toolbox.assetManager.finishLoadingAsset(mapPath);
     }
 
-    public TiledMap getMap(String mapPath) {
-        return Toolbox.assetManager.get(mapPath);
+    public static TiledMap getMap(String mapPath) {
+        return Toolbox.assetManager.get(mapPath, TiledMap.class);
     }
 
-    public void loadTexture(String texturePath) {
+    public static void loadTexture(String texturePath) {
         Toolbox.assetManager.load(texturePath, Texture.class);
     }
 
-    public Texture getTexture(String texturePath) {
+    public static Texture getTexture(String texturePath) {
         return Toolbox.assetManager.get(texturePath);
     }
 }
