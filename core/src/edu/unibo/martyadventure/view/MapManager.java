1d05b42138ffa23552a76606ebfa25ae12ed379e
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-17 16:19:37 +0200

    docs add javadoc comments to MapManager

--- core/src/edu/unibo/martyadventure/view/MapManager.java
+++ core/src/edu/unibo/martyadventure/view/MapManager.java
@@ -1,3 +1,4 @@
+//This class load the maps from the assets
 package edu.unibo.martyadventure.view;
 
 import java.util.Hashtable;
@@ -43,6 +44,8 @@ public class MapManager {
     private MapLayer enemySpawnLayer;
     private MapLayer biffSpawnLayer;
     
+    
+    //just initialize the hashtable
     public MapManager() {
         mapTable = new Hashtable<>();
         mapTable.put(MAP1, MAP1PATH);
@@ -50,17 +53,18 @@ public class MapManager {
         mapTable.put(MAP3, MAP3PATH);
     }
 
+    /** @return the current loaded map**/ 
     public TiledMap getCurrentMap() {
         return currentMap;
     }
-
+    /** @return the current loaded map name**/
     public String getCurrentMapName() {
         return currentMapName;
     }
-
+    /** @param the map name you want to load**/
     public void loadMap(String mapName) {
         
-        //get the map path and check it
+        //get the map path from the table and check it
         String mapPath = mapTable.get(mapName);
         if (mapPath.isEmpty()) {
             System.err.println("Map not loaded, invalid path");
@@ -123,27 +127,27 @@ public class MapManager {
         playerStartPosition.y = obj.getEllipse().y;
         
     }
-
+    /** @return the current loaded collision layer**/
     public MapLayer getCollisionLayer() {
         return collisionLayer;
     }
-
+    /** @return the player start position of the current map**/
     public Vector2 getPlayerStartPosition() {
         return playerStartPosition;
     }
-
+    /** @return the current loaded marty layer**/
     public MapLayer getMartySpawnLayer() {
         return martySpawnLayer;
     }
-
+    /** @return the current loaded pacman layer**/
     public MapLayer getPacManLayer() {
         return pacManLayer;
     }
-
+    /** @return the current loaded enemy spawn layer**/
     public MapLayer getEnemySpawnLayer() {
         return enemySpawnLayer;
     }
-
+    /** @return the current loaded biff layer**/
     public MapLayer getBiffSpawnLayer() {
         return biffSpawnLayer;
     }
