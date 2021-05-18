3297a4ecb987f276dd6e665396f12dc75f3de5cb
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-18 11:15:08 +0200

    fix desktop launcher code

--- desktop/src/edu/unibo/martyadventure/desktop/DesktopLauncher.java
+++ desktop/src/edu/unibo/martyadventure/desktop/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package edu.unibo.martyadventure.desktop;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import edu.unibo.martyadventure.MartyAdventureGame;
@@ -9,7 +8,6 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         
-        Lwjgl3Application app = new Lwjgl3Application(new MartyAdventureGame(), config);
-        Gdx.app = app;
+        new Lwjgl3Application(new MartyAdventureGame(), config);
     }
 }
