7aa5cafbf0fa569c6f1084d4d978abd678c11a46
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-31 12:39:40 +0200

    fix modified enemy character constructor

--- core/src/edu/unibo/martyadventure/model/character/EnemyCharacter.java
+++ core/src/edu/unibo/martyadventure/model/character/EnemyCharacter.java
@@ -1,13 +1,11 @@
 package edu.unibo.martyadventure.model.character;
 
-import com.badlogic.gdx.math.Vector2;
-
 import edu.unibo.martyadventure.model.weapon.Weapon;
 
 public class EnemyCharacter extends Character {
 
-    public EnemyCharacter(Weapon dropitem, String name, int hp, Weapon weapon, Vector2 position, float speed, Vector2 velocity) {
-        super(name, hp, weapon, position, speed, velocity);
+    public EnemyCharacter(Weapon dropitem, String name, int hp, Weapon weapon) {
+        super(name, hp, weapon);
         setDropitem(weapon);
     }
 
