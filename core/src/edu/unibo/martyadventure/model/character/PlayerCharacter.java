7bc20f6f630bd4e9a0474f8e63893ba6107410f0
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-05-31 12:37:51 +0200

    fix modified model player character constructor

--- core/src/edu/unibo/martyadventure/model/character/PlayerCharacter.java
+++ core/src/edu/unibo/martyadventure/model/character/PlayerCharacter.java
@@ -1,14 +1,11 @@
 package edu.unibo.martyadventure.model.character;
 
-import com.badlogic.gdx.math.Vector2;
-
 import edu.unibo.martyadventure.model.weapon.Weapon;
 
 public class PlayerCharacter extends Character {
     private Shoes shoes;
 
-    public PlayerCharacter(Shoes shoes, String name, int hp, Weapon weapon, Vector2 position, float speed,
-            Vector2 velocity) {
+    public PlayerCharacter(Shoes shoes, String name, int hp, Weapon weapon) {
         super(name, hp, weapon);
         setShoes(shoes);
     }
