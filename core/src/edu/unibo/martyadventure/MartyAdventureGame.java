24b0859ed67d6acfb1b8dd425708053f867d8ee2
Babini Stefano <stefano.babini5@studio.unibo.it>
2021-06-16 15:02:25 +0200

    fix remove unused import

--- core/src/edu/unibo/martyadventure/MartyAdventureGame.java
+++ core/src/edu/unibo/martyadventure/MartyAdventureGame.java
@@ -2,7 +2,6 @@ package edu.unibo.martyadventure;
 
 import com.badlogic.gdx.Game;
 
-import edu.unibo.martyadventure.view.MapManager;
 import edu.unibo.martyadventure.view.ScreenManager;
 
 public class MartyAdventureGame extends Game {
